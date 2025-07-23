eb4e6961ff3da1c82c8c18b822fa7ea52257b88a
Kurt Frey <kfrey@idmod.org>
2025-07-23 14:29:18 -0700

    Revert download scripts

--- model_polio_nga01/experiment_cVDPV2_100km_base_sia2p1-NGA_ri2027/make03_pool_brick.py
+++ model_polio_nga01/experiment_cVDPV2_100km_base_sia2p1-NGA_ri2027/make03_pool_brick.py
@@ -5,20 +5,15 @@
 import os
 import sys
 
-from idmtools.core.id_file import read_id_file
-
 # Ought to go in emodpy
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'local_python')))
-from emod_reduce import get_data_brick, pool_manager
-from py_assets_common.emod_constants import COMPS_ID_FILE
+from emod_reduce import get_data_brick
 
 # *****************************************************************************
 
 
 if (__name__ == "__main__"):
 
-    # get_data_brick()
-    (exp_id, _, _, _) = read_id_file(COMPS_ID_FILE)
-    pool_manager(exp_id)
+    get_data_brick()
 
 # *****************************************************************************
